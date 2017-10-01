61e4f9c354a524d733998aa6fd23e2cbdb98d87f
Coteika338 <goriainov02@mail.ru>
2017-10-01 13:52:48 +0400

    BG + SCRIPT

--- myTestProject/Assets/scripts/hodiBratan.cs
+++ myTestProject/Assets/scripts/hodiBratan.cs
@@ -7,10 +7,13 @@ public class hodiBratan : MonoBehaviour {
     float speedX;
     float verticalImpulse;
     Rigidbody2D rb;
+    bool isGrounded;
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-	}
+        horizontalSpeed = 0.1f;
+        verticalImpulse = 5;
+    }
 	
 	void FixedUpdate () {
         if (Input.GetKey(KeyCode.A)){
@@ -19,8 +22,25 @@ public class hodiBratan : MonoBehaviour {
             speedX = horizontalSpeed;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)){
-
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            rb.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
         }
+        transform.Translate(speedX,0,0);
+        speedX = 0;
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }
